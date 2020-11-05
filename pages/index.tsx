7774ecd2d3a67fd2b5413666bd8073b8beb30fc0
Welton Felix <contato.weltonf@gmail.com>
2020-11-05 20:55:47 -0300

    Cria a estrutura inicial do projeto
    Cria a estrutura do projeto utilizando o Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>IFPE Open Source</title>
+      </Head>
+
+      <main>
+        <h1>IFPE Open Source</h1>
+      </main>
+    </div>
+  )
+}
