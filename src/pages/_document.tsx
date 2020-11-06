b9e3f44ec2ebcd7e0c8d2a5e98628e92b8570575
Welton Felix <contato.weltonf@gmail.com>
2020-11-05 23:18:15 -0300

    Configura o Head da aplicação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -28,5 +35,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
