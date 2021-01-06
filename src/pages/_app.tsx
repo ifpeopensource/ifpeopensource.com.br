360168d67832c3e015f251eb07916eedc81255b8
Welton Felix <contato.weltonf@gmail.com>
2021-01-06 14:50:12 -0300

    Adiciona o Tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
+import 'tailwindcss/tailwind.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/pages/**/*.ts', './src/components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
