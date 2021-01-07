2140c88c5af2d12de78ba004b6f9c4699b545816
Welton Felix <contato.weltonf@gmail.com>
2021-01-07 10:40:58 -0300

    Adiciona esquema de cores

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,19 +1,9 @@
-import { createGlobalStyle, DefaultTheme } from 'styled-components';
-
-interface IThemeProps extends DefaultTheme {
-  theme: {
-    body: string;
-  };
-}
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-
-  body {
-    background: ${({ theme }: IThemeProps) => theme.body};
   }
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,19 @@
 export const darkTheme = {
+  primary: '#2F9E41',
+  secondary: '#C8191E',
   body: '#151514',
+  text: '#F5F5F5',
+  header: '#0A0A0A',
+  detail: '#20201D',
+  sectionAccent: '#303030',
 };
 
 export const lightTheme = {
+  primary: '#2F9E41',
+  secondary: '#C8191E',
   body: '#FBFBFB',
+  text: '#1C1E20',
+  header: '#FBFBFB',
+  detail: '#EBEBEB',
+  sectionAccent: '#F2F2F2',
 };
