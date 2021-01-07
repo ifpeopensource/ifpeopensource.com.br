cf386d027c218c3e112a61254d6e2d1eefc29a4c
Welton Felix <contato.weltonf@gmail.com>
2021-01-07 10:23:55 -0300

    Remove o Tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../styles/global';
 import { darkTheme } from '../styles/theme';
-import 'tailwindcss/tailwind.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={darkTheme}>
