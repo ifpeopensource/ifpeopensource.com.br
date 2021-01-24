12ea19555c74dcc338dee367037c3e033b3210d9
Welton Felix <contato.weltonf@gmail.com>
2021-01-24 10:50:24 -0300

    Adiciona favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,30 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color={theme.header}
+        />
+        <meta name="msapplication-TileColor" content={theme.header} />
         {theme === darkTheme ? (
           <meta name="color-scheme" content="dark" />
         ) : (
