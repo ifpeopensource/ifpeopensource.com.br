12eabcdf0afd51c93f05d97c9feb8a2e8c5b0139
Welton Felix <contato.weltonf@gmail.com>
2021-04-15 19:39:57 -0300

    Corrige erros gramaticais no componente "WhoWeAre"

--- src/components/WhoWeAre/index.tsx
+++ src/components/WhoWeAre/index.tsx
@@ -16,10 +16,10 @@ const WhoWeAre: React.FC = () => {
       <Description style={{ textIndent: '2.5rem' }}>
         Somos uma comunidade de alunos interessados em computação, programação,
         cultura maker e tecnologia em geral que promove projetos onde podemos
-        aproveitar o enorme potencial que a comunidade de alunos do IFPE têm de
+        aproveitar o enorme potencial que a comunidade de alunos do IFPE tem de
         usar a tecnologia para criar soluções e compartilhá-las com os outros
         para modificar e melhorar o nosso ambiente. Junto a isso, levamos todo
-        o conhecimento, experiência e networking que uma comunidade deste tipo
+        o conhecimento, experiência e networking que uma comunidade desse tipo
         pode proporcionar.
       </Description>
       <ReadMoreButton onClick={toggleModal}>Ler mais</ReadMoreButton>
