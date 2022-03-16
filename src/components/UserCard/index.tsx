c647110c9b9a0ed18f6ee8f34d5b753fa4d9755b
Ryan Gomes Paiva <ryangpaiva@MacBook-Air-de-Ryan.local>
2022-03-16 00:19:57 -0300

    corrige eslint

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -32,7 +32,11 @@ const UserCard: React.FC<UserCardProps> = ({ user, isShort }) => {
     <Card>
       <UserAvatar src={user.avatarUrl} />
       <UserName>{user.name}</UserName>
-      <GhUserContainer href={`https://github.com/${user.ghUsername}`} target="_blank" rel="noopener noreferrer">
+      <GhUserContainer
+        href={`https://github.com/${user.ghUsername}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaGithub size={20} />
         <span>{user.ghUsername}</span>
       </GhUserContainer>
