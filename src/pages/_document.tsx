74bfad6ff23331e183e6ab43e0a2ae8caffc6cfa
Welton Felix <contato.weltonf@gmail.com>
2022-04-22 00:51:59 -0300

    Corrige erros da atualização
    Erros causados pelo @types/react=^18.0.6

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import { Fragment } from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,12 +26,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
+        styles: [
+          <Fragment key={1}>
             {initialProps.styles}
             {sheet.getStyleElement()}
-          </>
-        ),
+          </Fragment>,
+        ],
       };
     } finally {
       sheet.seal();
