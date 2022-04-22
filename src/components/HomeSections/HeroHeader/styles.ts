0b88619c1ff084ebf4b0743b401a94f76447577e
Welton Felix <contato.weltonf@gmail.com>
2022-04-22 10:34:59 -0300

    Ignora diretórios no ESLint e no Prettier

--- src/components/HomeSections/HeroHeader/styles.ts
+++ src/components/HomeSections/HeroHeader/styles.ts
@@ -10,9 +10,9 @@ export const Container = styled.div`
   display: flex;
 
   background: ${(props) =>
-    (props.theme === darkTheme
-      ? 'linear-gradient(90deg, rgba(0, 0, 0, 0.69) 0%, rgba(0, 0, 0, 0.46) 100%)'
-      : 'linear-gradient(90deg, rgba(255, 255, 255, 0.62) 0%, rgba(255, 255, 255, 0.25) 100%)')},
+      props.theme === darkTheme
+        ? 'linear-gradient(90deg, rgba(0, 0, 0, 0.69) 0%, rgba(0, 0, 0, 0.46) 100%)'
+        : 'linear-gradient(90deg, rgba(255, 255, 255, 0.62) 0%, rgba(255, 255, 255, 0.25) 100%)'},
     url('/assets/images/IFPE-Light.jpg') no-repeat;
   background-size: cover;
   background-position: bottom;
